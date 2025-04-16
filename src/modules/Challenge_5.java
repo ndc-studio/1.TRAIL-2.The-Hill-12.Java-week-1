1ffa0de60afd99870a03a7e13e9d05ee66940bd5
ndc-studio <ndc.dev.code@gmail.com>
2025-04-16 17:11:08 +0200

    Start Challenge_5

--- src/modules/Challenge_5.java
+++ src/modules/Challenge_5.java
@@ -1,5 +1,22 @@
 package modules;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Challenge_5 {
-    
+    public static void challenge_5() {
+        try {
+            File myObj = new File("./src/data/data.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
