5bfb98c26baf81a11822e11064b7571ad5c2b7e3
ndc-studio <ndc.dev.code@gmail.com>
2025-04-17 14:22:17 +0200

    PersonClass Finished

--- src/modules/PersonClass.java
+++ src/modules/PersonClass.java
@@ -11,5 +11,32 @@ public class PersonClass {
         this.age = age;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person: " + firstname + ", " + lastname + ", " + age + ".\n";
+    }
     
 }
