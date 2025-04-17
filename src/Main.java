db1117d313ee5c0831f9b1f2535baf2024a2995e
ndc-studio <ndc.dev.code@gmail.com>
2025-04-17 11:45:23 +0200

    Challenge_6 Finished

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import modules.*;
 
 public class Main {
     // Change pour voir chaque exercices exemple 1 = Challenge_1
-    static int exercice = 5;
+    static int exercice = 6;
 
     public static void main(String[] args) {
         switch (exercice) {
@@ -12,15 +12,12 @@ public class Main {
             case 2:
                 Challenge_2.challenge_2();
                 break;
-        
             case 4:
                 Challenge_4.challenge_4();
                 break;
-        
             case 5:
                 Challenge_5.challenge_5();
                 break;
-        
             case 6:
                 Challenge_6.challenge_6();
                 break;
--- src/modules/Challenge_6.java
+++ src/modules/Challenge_6.java
@@ -1,7 +1,47 @@
 package modules;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashMap;
+
+/**
+ * @see READ: https://www.w3schools.com/java/java_files_read.asp
+ */
 public class Challenge_6 {
     public static void challenge_6() {
+        try {
+            File myObj = new File("./src/data/data.txt");
+            Scanner myReader = new Scanner(myObj);
+            HashMap<Character, Integer> counterMap = new HashMap<Character, Integer>();
 
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String regex = "[^a-zA-Z]";
+                String str = data.replaceAll(regex, "").toLowerCase();
+
+                
+                for (int i = 0; i < str.length(); i++) {
+                    char charCurrentI = str.charAt(i);
+
+                    
+                    for (int j = 0; j < str.length(); j++) {
+                        char charCurrentJ = str.charAt(j);
+
+                        if (charCurrentI == charCurrentJ) {
+                            counterMap.put(charCurrentI, counterMap.getOrDefault(charCurrentI, 0) + 1);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(counterMap);
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("SCRIPT_ERROR: An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
