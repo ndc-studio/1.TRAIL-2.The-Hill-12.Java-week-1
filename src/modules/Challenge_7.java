f2edb27c64373df1b361fccd2a1df8acda2da018
ndc-studio <ndc.dev.code@gmail.com>
2025-04-17 12:31:19 +0200

    Create file PersonClass.java and start to write class

--- src/modules/Challenge_7.java
+++ src/modules/Challenge_7.java
@@ -19,5 +19,4 @@ public class Challenge_7 {
           e.printStackTrace();
         }
     }
-    
 }
---
+++ src/modules/PersonClass.java
@@ -0,0 +1,15 @@
+package modules;
+
+public class PersonClass {
+    private String firstname;
+    private String lastname;
+    private Integer age;
+
+    public PersonClass(String firstname, String  lastname, Integer age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    
+}
