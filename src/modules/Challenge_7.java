211a7ea47a68b9013aa8619f47d7cac254811580
ndc-studio <ndc.dev.code@gmail.com>
2025-04-17 14:39:07 +0200

    Modif data.csv > data-C7.csv & Prépare Challenge_8. Create Challenge_8.java and data-C8.csv

--- src/modules/Challenge_7.java
+++ src/modules/Challenge_7.java
@@ -7,12 +7,23 @@ import java.util.Scanner;
 public class Challenge_7 {
     public static void challenge_7() {
         try {
-            File myObj = new File("./src/data/data.csv");
+            File myObj = new File("./src/data/data-C7.csv");
             Scanner myReader = new Scanner(myObj);
+            myReader.nextLine();
+
             while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            System.out.println(data);
-        }
+                String data = myReader.nextLine();
+                // Debug
+                System.out.println(data);
+                String[] cut = data.split(",");
+                String firstname = cut[0].trim();
+                String lastname = cut[1].trim();
+                int age = Integer.parseInt(cut[2].trim());
+
+                PersonClass person = new PersonClass(firstname, lastname, age);
+                System.out.print(person);
+
+            }
           myReader.close();
         } catch (FileNotFoundException e) {
           System.out.println("An error occurred.");
@@ -20,3 +31,4 @@ public class Challenge_7 {
         }
     }
 }
+
---
+++ src/modules/Challenge_8.java
@@ -0,0 +1,7 @@
+package modules;
+
+public class Challenge_8 {
+    public static void challenge_8() {
+        
+    }
+}
