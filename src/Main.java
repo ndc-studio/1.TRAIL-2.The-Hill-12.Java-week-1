adba34efaa32d35eae2c31e41aed29fb89f4881a
pc-portable <martin.stephan9218@gmail.com>
2025-04-18 10:47:19 +0200

    delete data day.csv & month.csv

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import modules.*;
 
 public class Main {
     // Change la valeur pour voir chaque exercices exemple 1 = Challenge_1
-    static int exercice = 8;
+    static int exercice = 9;
 
     public static void main(String[] args) {
         switch (exercice) {
