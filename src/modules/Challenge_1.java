0404312d41ce9ae11822062037a8194089fc977b
ndc-studio <ndc.dev.code@gmail.com>
2025-04-22 12:11:18 +0200

    Extra Challenge_1 Finished

--- src/modules/Challenge_1.java
+++ src/modules/Challenge_1.java
@@ -21,3 +21,24 @@ public class Challenge_1 {
         System.out.println("boolean: " + boolType);
     }
 }
+
+/* 
+
+EXTRA:
+Question 1: Pourquoi peut-on afficher tous ces types de données aussi facilement ?
+Réponse 1: Les valeurs des variables sont appellées dans un print sous le nom de la variable precité, le print affiche directement le resultat sur le terminal quand on appelle challenge_1() dans le Main.Java.
+
+Question 2: Pourquoi les appelle-t-on des types primitifs ?
+Réponse 2: Ils ne peuvent contenir que des valeurs limitées et que nous ne pouvons pas definir de nouvelles opérations avec ces valeurs
+
+Question 3: Pourquoi ne peut-on pas affecter un double à un int ?
+Réponse 3: Le Double es tun chiffre a virgule (décimales) tandis que le int est un nombre entier donc java ne permet pas d'affecter un double en int pour se faire il faudra le transformer en int au préalable.
+
+Question 4: Mais alors, pourquoi peut-on affecter un int à un char ?
+Réponse 4: On va dire qu'un char (character) peut etre converti en serie de chiffre unicode en gros si on affecte un int a un char java pourra le retranscrire
+
+Question 5: Pourquoi ne peut-on pas affecter un long à un int ?
+Réponse 5: Si j'ai bien compris les primitifs sont contenus dans un espace en bits et certaines variables sont en 2, 4, 8, 16bits etc le long luii utilise 64bits et le int seulement 32
+ce qui ne permet pas a un "int" de contenir la longueur d'un "Long"
+
+*/
